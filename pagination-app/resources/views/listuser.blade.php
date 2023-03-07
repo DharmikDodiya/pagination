49298e7b1ead0c382d455f50d0b1db1f5aaa9788
dharmik <dharmikd@zignuts.com>
2023-03-07 19:29:13 +0530

    create pagination app

---
+++ pagination-app/resources/views/listuser.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+    <html>
+
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Laravel 8 Pagination Demo - codeanddeploy.com</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+    </head>
+
+    <body>
+        <div class="container mt-5">
+            <table class="table table-success table-hover">
+                <thead>
+                <tr style="background-color: cadetblue">
+                    <th scope="col" width="1%">#</th>
+                    <th scope="col" width="15%">Name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col" width="10%">Username</th>
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach($users as $user)
+                        <tr>
+                            <th scope="row">{{ $user->id }}</th>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->username }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+
+            <div class="d-flex">
+                {!! $users->links() !!}
+            </div>
+        </div>
+
+        <div>
+            <ul class="pagination">
+                <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+                <li class="active"><a href="#!">1</a></li>
+                <li class="waves-effect"><a href="#!">2</a></li>
+                <li class="waves-effect"><a href="#!">3</a></li>
+                <li class="waves-effect"><a href="#!">4</a></li>
+                <li class="waves-effect"><a href="#!">5</a></li>
+                <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+              </ul>
+        </div>
+    </body>
+</html>
